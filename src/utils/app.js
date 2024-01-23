d8bd83fcdcdd653b12363742ccbe5d6af7839a1c
MFahim786 <132273833+MFahim786@users.noreply.github.com>
2024-01-23 19:52:50 +0500

    Update app.js

--- src/utils/app.js
+++ src/utils/app.js
@@ -19,4 +19,9 @@ app.use(cookieParser())
 //routes import
 app.use("/api/videoupload", videoUpload)
 app.use("/api/fetchvideo",fetchVideo)
+app.use((req, res, next) => { 
+    res.status(404).json({
+      error: 'Bad Request'
+    });
+  });
 export { app }
