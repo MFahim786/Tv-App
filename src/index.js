424c3840353a607d17baba0cedd0f3bde483e633
MFahim786 <132273833+MFahim786@users.noreply.github.com>
2024-01-24 17:20:47 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,50 +1,15 @@
-
-const mongoose = require("mongoose");
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express();
-const cors=require('cors');
-const port = 3000;
-const { runMiddleware, handleImageUpload } = require("./Api/Routes/imageupload");
-const user=require('./Api/Routes/user_api')
-const social_media=require('./Api/Routes/social_media')
-const userImage = require('./Api/Routes/index');
-const flickCode=require('./Api/Routes/flickCodes')
-const bodyparser=require('body-parser');
-app.use(bodyparser.urlencoded({extended:false}));
-app.use(bodyparser.json());
-app.use('/user', user); 
-app.use('/socialmedia', social_media);
-app.use('/UserImg', userImage);
-app.use('/flickCode',flickCode);
-app.use(express.static('public'));
-app.use(cors());
-app.post("/UserImg/uploadImage/:id", async (req, res) => {
-  await handleImageUpload(req, res);
-});
- app.use((req, res, next) => { 
-  res.status(404).json({
-    error: 'Bad Request'
-  });
-});
- 
-const uri = "mongodb+srv://mfahimch:78678PPIred@cluster0.eviduti.mongodb.net/?retryWrites=true&w=majority";
-async function run() {
-  try {
-    const connectionString = "mongodb://localhost:27017/test";
-
-    await mongoose.connect(uri, {
-      useNewUrlParser: true, 
-      useUnifiedTopology: true,
-    }).then(data => {
-      console.log('data');
-    });
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-run();
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
