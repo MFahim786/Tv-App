1549da75d0f90a5423d70bc26c1312fee7c4adb9
MFahim786 <132273833+MFahim786@users.noreply.github.com>
2024-02-14 23:29:12 +0500

    Update fetchvideo.routes.js

--- src/routes/fetchvideo.routes.js
+++ src/routes/fetchvideo.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import {fetchVideo} from "../controllers/client/fetchvideo"
+import {fetchVideo, fetchVideoCategorey} from "../controllers/client/fetchvideo"
 import {videoUpload} from "../controllers/admin/uploadvideo"
 import {deletevideo} from "../controllers/admin/deletevideo"
-import {matchcatergorey} from "../controllers/client/categorey"
+import {catergoreylist, matchcatergorey} from "../controllers/client/categorey"
 import {addcategorey} from "../controllers/client/categorey"
 const router = Router();
 router.route("/"),get(fetchVideo);
@@ -10,3 +10,5 @@ router.route('/').post(videoUpload);
 router.route('/').delete(deletevideo); 
 router.route('/').post(matchcatergorey); 
 router.route('/').post(addcategorey); 
+router.route('/').get(catergoreylist); 
+router.route('/').post(fetchVideoCategorey); 
